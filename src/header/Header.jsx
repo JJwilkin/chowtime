5301890d7b3ef8914b6a58aed36391ff93e087c0
Joshua Wilkinson <joshuawilkinson@Joshuas-MacBook-Air.local>
2020-01-18 18:06:56 -0500

    frame

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './Header.scss'
 
 export default function Header () {
     
     
     return( <>
-        <h1> Hello world</h1>
+        <h1 class='intro'> Hello world</h1>
 
     </>
 
