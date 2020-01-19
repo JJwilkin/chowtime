ec8533daf2f319cc6e030949e9d7d4dd2f0d9b4e
Joshua Wilkinson <joshuawilkinson@Joshuas-MacBook-Air.local>
2020-01-19 13:50:50 -0500

    changes

--- src/foodlist/foodlist.jsx
+++ src/foodlist/foodlist.jsx
@@ -4,13 +4,30 @@ import './foodlist.css'
 export default function FoodList (props) {
     // console.log(props.foodlist)
   return(
-    <>
+
+    <div class='foodlist'>
     {
       props.foodlist.forEach((item) => {
         console.log(item)
         // <h1>{ item }</h1>
       })
     }
-    </>
+
+    <div class='row'>
+      <div class='col-8 bottom-border'><h2>Test</h2></div>
+      <div class='col-4'>Box</div>
+    </div>
+
+    <div class='row'>
+      <div class='col-8 bottom-border'><h2>Test</h2></div>
+      <div class='col-4'>Box</div>
+    </div>
+    <div class='row'>
+      <div class='col-8 bottom-border'><h2>Test</h2></div>
+      <div class='col-4'>Box</div>
+    </div>
+  
+   
+    </div>
   )
 }
