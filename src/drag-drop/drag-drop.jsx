6e7a936f48a610528ef14fc4c3e8aab50a02c488
Aliya Rajwani <rajwani.aliya@gmail.com>
2020-01-19 01:44:03 -0500

    updated drag and drop

--- src/drag-drop/drag-drop.jsx
+++ src/drag-drop/drag-drop.jsx
@@ -7,13 +7,17 @@ export default function DragDrop () {
     <div class="view-wrap">
       <div class="text">
         <h5>Upload Image</h5>
-        <p>Take a photo of your fridge and start ake a photo of your fridge and start ake a photo of your fridge and.</p>
+        <p>Snap a clear photo of the items you'd like to use in your dish to get started.</p>
       </div>
       <div class="box">
         <div class="input-holder">
           <button onClick={ () => document.getElementById('getFile').click()}><img class="upload" src="./assets/upload-2.png"></img></button>
           <input class="hide" id="getFile" type="file" accept="image/png, image/jpeg"></input>
         </div>
+      </div>
+      <div class="row">
+        <button class="col confirm">Confirm</button>
+        <button class="col confirm">Upload New</button>
       </div>
     </div>
   )
