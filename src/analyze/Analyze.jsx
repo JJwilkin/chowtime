4efbf0f9278ef7d10f405beb018ec8975a8614ea
Joshua Wilkinson <joshuawilkinson@Joshuas-MacBook-Air.local>
2020-01-19 13:38:27 -0500

    changes

--- src/analyze/Analyze.jsx
+++ src/analyze/Analyze.jsx
@@ -7,8 +7,23 @@ export default function Anaylze () {
 
   return(
     <div class="view-wrap">
-      
+    <div class="text">
+      <h5>Upload Image</h5>
+      <p>Snap a clear photo of the items you'd like to use in your dish to get started.</p>
     </div>
+    <div class="box">
+      {/* <div class={`input-holder ${uploadedImage ? 'blank': ''}`}> */}
+        {/* <button onClick={ () => document.getElementById('getFile').click()}><img class={`upload ${uploadedImage ? 'new_image' : ''}`} src="./assets/upload-2.png"></img></button> */}
+        
+      {/* </div> */}
+    </div> 
+    <div class="row">
+    <Link to={`/process`} >
+      <button class="col confirm" >Confirm</button> 
+      </Link>
+      <button class="col confirm" onClick={ () => document.getElementById('getFile').click()}>Upload New</button>
+    </div>
+  </div>
   )
 
 
